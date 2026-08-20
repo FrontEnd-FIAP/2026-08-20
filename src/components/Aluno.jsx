752159088a1ade6d5cf4b5e7a645488cd09bc42b
Eduardo Craveiro <eduardo.craveiro26@gmail.com>
2026-08-20 08:53:02 -0300

    feat: Componente utilizando props

---
+++ src/components/Aluno.jsx
@@ -0,0 +1,12 @@
+
+
+const Aluno = (props) => {
+  return (
+    <>
+    <p>O Nome do aluno é: {props.nome}</p>
+    <p>A Idade do aluno é: {props.idade}</p>
+    </>
+  )
+}
+
+export default Aluno
